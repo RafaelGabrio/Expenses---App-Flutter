096088ef6a3c7d8a7a0b83cb7f2ea50fa2c707e4
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-09-23 12:17:56 -0300

    Ajustando espaçamento dos componentes em MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,10 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: double.infinity,
             child: Card(
               color: Colors.blue,
               child: Text('Gráfico'),

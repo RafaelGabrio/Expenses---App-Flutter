f8649c70352ad63bb5f39e6ad163d9ae1cf600f9
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-09-23 12:25:36 -0300

    Criação de listas de transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 main() => runApp(ExpensesApp());
 
@@ -12,6 +13,20 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now()
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {

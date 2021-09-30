b5f6aa36e9d5af441de32f5d8cbf199433dcb941
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-09-30 11:34:06 -0300

    validação dos campos titulo e valor em TransactionForm

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,6 +8,17 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if(title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,12 +29,15 @@ class TransactionForm extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
@@ -36,11 +50,7 @@ class TransactionForm extends StatelessWidget {
                     primary: Colors.purple
                   ),
                   child: Text('Nova Transação'),
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
+                  onPressed: _submitForm,
                 ),
               ],
             ),
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -25,24 +25,6 @@ class _TransactionUserState extends State<TransactionUser> {
         value: 211.30, 
         date: DateTime.now(),
     ),
-    Transaction(
-        id: 't2', 
-        title: 'Conta de Luz', 
-        value: 211.30, 
-        date: DateTime.now(),
-    ),
-    Transaction(
-        id: 't2', 
-        title: 'Conta de Luz', 
-        value: 211.30, 
-        date: DateTime.now(),
-    ),
-    Transaction(
-        id: 't2', 
-        title: 'Conta de Luz', 
-        value: 211.30, 
-        date: DateTime.now(),
-    ),
   ];
 
   _addTransaction(String title, double value) {

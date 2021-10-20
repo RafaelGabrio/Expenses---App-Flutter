c2b9c7e87b2f63410c4bc717e39b5f598d13e54a
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-10-20 15:23:05 -0300

    ajuste de signe no gráfico

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -30,7 +30,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay)[0],
         'value': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get _weekTotalValue {

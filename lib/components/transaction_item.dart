02906cbc53bd0d9ed28a4b4cb5bf1c11b9202c09
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-11-16 12:30:15 -0300

    inserção de cor de fundo randomica em Transactiontem

--- lib/components/transaction_item.dart
+++ lib/components/transaction_item.dart
@@ -1,9 +1,11 @@
 
+import 'dart:math';
+
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   final Transaction tr;
   final void Function(String p1) onRemove;
   
@@ -12,6 +14,29 @@ class TransactionItem extends StatelessWidget {
     required this.tr,
     required this.onRemove,
   }) : super(key: key);
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+
+  static const colors = [
+    Colors.red,
+    Colors.purple,
+    Colors.orange,
+    Colors.blue,
+    Colors.black
+  ];
+
+  late Color _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    int i = Random().nextInt(5);
+    _backgroundColor = colors[i];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,31 +48,32 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _backgroundColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(9),
             child: FittedBox(
-              child: Text('R\$${tr.value}'),
+              child: Text('R\$${widget.tr.value}'),
             ),
           ),
         ),
-        title: Text(tr.title,
+        title: Text(widget.tr.title,
             style: Theme.of(context).textTheme.headline6),
         subtitle: Text(
-          DateFormat('d MMM y').format(tr.date),
+          DateFormat('d MMM y').format(widget.tr.date),
         ),
         trailing: MediaQuery.of(context).size.width > 480
             ? TextButton.icon(
                 label: const Text('Excluir'),
                 icon: const Icon(Icons.delete),
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
                 style: TextButton.styleFrom(
                   primary: Theme.of(context).errorColor,
                 ),
               )
             //  ),
             : IconButton(
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
               ),
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -32,15 +32,26 @@ class TransactionList extends StatelessWidget {
               );
             },
           )
-        : ListView.builder(
-            itemCount: transactions.length,
-            itemBuilder: (ctx, index) {
-              final tr = transactions[index];
+        : ListView(
+          children: transactions.map(
+            (tr) {
               return TransactionItem(
+                key: ValueKey(tr.id),
                 tr: tr,
                 onRemove: onRemove,
               );
-            },
-          );
+            }
+          ).toList(),
+        );
+        // : ListView.builder(
+        //     itemCount: transactions.length,
+        //     itemBuilder: (ctx, index) {
+        //       final tr = transactions[index];
+        //       return TransactionItem(
+        //         tr: tr,
+        //         onRemove: onRemove,
+        //       );
+        //     },
+        //   );
   }
 }
